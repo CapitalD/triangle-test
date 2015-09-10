dc6edae3360c7ef8d3e46e72f1e72d74c5464397
Duncan Thurston <duncan@capitald.co.nz>
2015-09-10 16:15:01 +1200

    List Tasters

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var admin = require('./routes/admin');
-var users = require('./routes/users');
+var tasters = require('./routes/tasters');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/admin', admin);
-app.use('/users', users);
+app.use('/tasters', tasters);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
