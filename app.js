def492c185f2beebb8e1a5b98581c8713f63054a
Duncan Thurston <duncan@capitald.co.nz>
2015-09-20 13:26:55 +1200

    Added express-validator with basic validations kinda working, sort of...

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var validator = require('express-validator');
 
 var routes = require('./routes/index');
 var admin = require('./routes/admin');
@@ -21,6 +22,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/tasters.js
+++ routes/tasters.js
@@ -16,6 +16,28 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/new', function(req, res, next) {
+
+  req.checkBody('name_field', "Your name is required").notEmpty();
+  req.checkBody('email_field', "Your email address isn't valid").optional().isEmail();
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.send(errors);
+  } else {
+    models.Taster.create({
+      name: req.body.name_field,
+      email: req.body.email_field,
+      BrewingExperienceId: req.body.brewing_exp_field,
+      CertificationId: req.body.certification_field,
+      overall_knowledge: req.body.overall_knowledge_field
+    }).then(function() {
+      res.redirect('/tasters');
+    });
+  }
+});
+
 router.get('/new', function(req, res, next) {
   models.BrewingExperience.findAll().then(function(experiences) {
     models.Certification.findAll().then(function(certifications) {
@@ -25,19 +47,6 @@ router.get('/new', function(req, res, next) {
         certifications: certifications
       });
     });
-  });
-});
-
-router.post('/new', function(req, res, next) {
-  
-  models.Taster.create({
-    name: req.body.name_field,
-    email: req.body.email_field,
-    BrewingExperienceId: req.body.brewing_exp_field,
-    CertificationId: req.body.certification_field,
-    overall_knowledge: req.body.overall_knowledge_field
-  }).then(function() {
-    res.redirect('/tasters');
   });
 });
 
