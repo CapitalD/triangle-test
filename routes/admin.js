6d7801fc7312fb5a4cea35bc7ffd577bb357775c
Duncan Thurston <duncan@capitald.co.nz>
2015-09-21 01:10:48 +1200

    active count on admin home

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var models = require('../models');
 var router = express.Router();
+
+
 
 /* GET admin home. */
 router.get('/', function(req, res, next) {
-  res.render('admin-home', { title: "Triangle Test Administration" });
+  models.Test.count({
+    where: ["in_progress = true"]
+  }).then(function(active_test_count) {
+    res.render('admin-home', {
+      title: "Triangle Test Administration",
+      active_tests: active_test_count
+    });
+  });
 });
 
 module.exports = router;
