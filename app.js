9c5e51694107aa1eaa3ab7e6b5af39f0e15ed68d
Duncan Thurston <duncan@capitald.co.nz>
2015-09-21 14:35:48 +1200

    included pluralize and used for active test count on admin home

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.locals.moment = require('moment');
+app.locals.pluralize = require('pluralize');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
